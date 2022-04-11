b412a51493bb98cbad0cae6625afea0544561e67
OxygenJam <triztandriz@gmail.com>
2022-04-11 22:01:09 +0800

    jolly jolly jollibee happy

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import jabee from "../../assets/jabee.jpg"
+
+import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+	return (
+		<React.Fragment>
+			<header className={classes.header}>
+                <h1>Jollibee</h1>
+                <HeaderCartButton/>
+            </header>
+			<div className={classes['main-image']}>
+                <img src={jabee} alt="Jollihotdog masarap huehuehueh"/>
+            </div>
+		</React.Fragment>
+	);
+};
+
+export default Header;
